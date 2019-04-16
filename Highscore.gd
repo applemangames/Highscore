29b64d175c2c78ab9710697d156f7faf7946cd65
Martin Jablecnik <martin.jablecnik@email.cz>
2019-04-16 21:26:26 +0200

    small refactoring

--- Highscore.gd
+++ Highscore.gd
@@ -6,24 +6,24 @@ var score_array = [1000, 2000, 5000, 10000, 20000, 30000, 1000, 2000, 5000, 1000
 var subLabel = load("res://SubLabel.tscn")
 
 func _ready():
-	var positionsContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Positions
-	var namesContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Names
-	var scoresContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Scores
-	
-	for i in names_array.size():
-		var position = subLabel.instance()
-		position.text = str(i+1)
-		
-		var label = subLabel.instance()
-		label.text = names_array[i]
-		
-		var score = subLabel.instance()
-		score.text = str(score_array[i])
-		
-		
-		positionsContainer.add_child(position) 
-		namesContainer.add_child(label) 
-		scoresContainer.add_child(score) 
-		
-	
+    var positionsContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Positions
+    var namesContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Names
+    var scoresContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Scores
+    
+    for i in names_array.size():
+        var position = subLabel.instance()
+        position.text = str(i+1)
+        
+        var label = subLabel.instance()
+        label.text = names_array[i]
+        
+        var score = subLabel.instance()
+        score.text = str(score_array[i])
+        
+        
+        positionsContainer.add_child(position) 
+        namesContainer.add_child(label) 
+        scoresContainer.add_child(score) 
+        
+    
 
